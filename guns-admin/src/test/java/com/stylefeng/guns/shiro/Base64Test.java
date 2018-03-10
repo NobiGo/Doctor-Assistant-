59c117845768c610d0253fb527b2e4d6138c90d9
dx <nobigogle@gmail.com>
2018-03-10 15:07:18 +0800

    添加 修改 病人情况

--- guns-admin/src/test/java/com/stylefeng/guns/shiro/Base64Test.java
+++ guns-admin/src/test/java/com/stylefeng/guns/shiro/Base64Test.java
@@ -6,7 +6,6 @@ import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 
 public class Base64Test {
-
     /**
      * Shiro 记住密码采用的是AES加密，AES key length 需要是16位，该方法生成16位的key
      */
@@ -21,7 +20,5 @@ public class Base64Test {
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		}
-        
     }
-
 }
