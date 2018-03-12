2eb400eb181caaed5c77eda3a0f9e3f63c23a0d4
dx <nobigogle@gmail.com>
2018-03-12 16:52:12 +0800

    样式确定

--- guns-admin/src/main/java/com/stylefeng/guns/Test.java
+++ guns-admin/src/main/java/com/stylefeng/guns/Test.java
@@ -1,10 +1,8 @@
 package com.stylefeng.guns;
 
-import java.text.DateFormat;
+import com.zte.datamask.name.ChinaNameMask;
+
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
 
 /**
  * Created by dx on 2018/3/11.
@@ -16,9 +14,15 @@ public class Test {
 //        Date date = simples.parse(hql);
 //        SimpleDateFormat simple = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 //        System.out.println(simple.format(date));
-        Date now=new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String tablename=dateFormat.format(now);
-        System.out.println(tablename);
+//        Date now=new Date();
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        String tablename=dateFormat.format(now);
+//        System.out.println(tablename);
+        maskTest();
+    }
+
+    public static void maskTest() {
+        //  return AccountMask.masking_account_info_caesarcipher(value, flag);
+        System.out.println(ChinaNameMask.masking_china_name_full_hash("段雄", "1"));
     }
 }
